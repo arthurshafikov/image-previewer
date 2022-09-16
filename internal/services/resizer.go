f78b6211fc14586ad8c57506502b588b8a827dd9
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-16 08:24:29 +0400

    add additional unit test to resizerService

--- internal/services/resizer.go
+++ internal/services/resizer.go
@@ -29,33 +29,7 @@ func (rs *ResizerService) ResizeFromURL(inp core.ResizeInput) (*os.File, error) 
 	resizedImage, err := rs.resizedImageCache.Remember(
 		fmt.Sprintf("%s_%vx%v", inp.ImageURL, inp.Width, inp.Height),
 		func() (*core.Image, error) {
-			image, err := rs.rawImageCache.Remember(inp.ImageURL, func() (*core.Image, error) {
-				return rs.imagesService.DownloadFromURLAndSaveImageToStorage(core.DownloadImageInput{
-					URL:    inp.ImageURL,
-					Header: inp.Header,
-				})
-			})
-			if err != nil {
-				return nil, err
-			}
-
-			resizedThumbnail, err := image.Crop(inp.Width, inp.Height)
-			if err != nil {
-				return nil, err
-			}
-
-			resizedFile, err := rs.imagesService.SaveResizedImageToStorage(
-				image.GetFullNameWithWidthAndHeight(inp.Width, inp.Height),
-				resizedThumbnail,
-			)
-			if err != nil {
-				return nil, err
-			}
-
-			return &core.Image{
-				File:         resizedFile,
-				DecodedImage: resizedThumbnail,
-			}, nil
+			return rs.downloadImageAndResize(inp)
 		},
 	)
 	if err != nil {
@@ -63,4 +37,34 @@ func (rs *ResizerService) ResizeFromURL(inp core.ResizeInput) (*os.File, error) 
 	}
 
 	return resizedImage.File, nil
+}
+
+func (rs *ResizerService) downloadImageAndResize(inp core.ResizeInput) (*core.Image, error) {
+	image, err := rs.rawImageCache.Remember(inp.ImageURL, func() (*core.Image, error) {
+		return rs.imagesService.DownloadFromURLAndSaveImageToStorage(core.DownloadImageInput{
+			URL:    inp.ImageURL,
+			Header: inp.Header,
+		})
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resizedThumbnail, err := image.Crop(inp.Width, inp.Height)
+	if err != nil {
+		return nil, err
+	}
+
+	resizedFile, err := rs.imagesService.SaveResizedImageToStorage(
+		image.GetFullNameWithWidthAndHeight(inp.Width, inp.Height),
+		resizedThumbnail,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &core.Image{
+		File:         resizedFile,
+		DecodedImage: resizedThumbnail,
+	}, nil
 }
--- internal/services/resizer_test.go
+++ internal/services/resizer_test.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"image"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/arthurshafikov/image-previewer/internal/core"
@@ -9,6 +11,13 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
+
+var input = core.ResizeInput{
+	ImageURL: someImageURL,
+	Width:    200,
+	Height:   500,
+	Header:   http.Header{},
+}
 
 func TestResizeFromURL(t *testing.T) {
 	ctl := gomock.NewController(t)
@@ -18,20 +27,33 @@ func TestResizeFromURL(t *testing.T) {
 	resizerService := NewResizerService(rawImageCacheMock, resizedImageCacheMock, imagesServiceMock)
 
 	gomock.InOrder(
-		resizedImageCacheMock.EXPECT().
-			Remember(
-				"https://some-url.com/some-image.jpg_200x500",
-				gomock.AssignableToTypeOf(func() (*core.Image, error) { return nil, nil }),
-			).
-			Times(1).
-			Return(coreImage, nil),
+		resizedImageCacheMock.EXPECT().Remember(
+			"https://some-url.com/some-image.jpg_200x500",
+			gomock.AssignableToTypeOf(func() (*core.Image, error) { return nil, nil }),
+		).Return(coreImage, nil),
 	)
 
-	_, err := resizerService.ResizeFromURL(core.ResizeInput{
-		ImageURL: someImageURL,
-		Width:    200,
-		Height:   500,
-		Header:   http.Header{},
-	})
+	_, err := resizerService.ResizeFromURL(input)
 	require.NoError(t, err)
+}
+
+func TestDownloadImageAndResize(t *testing.T) {
+	ctl := gomock.NewController(t)
+	rawImageCacheMock := mock_services.NewMockImageCache(ctl)
+	resizedImageCacheMock := mock_services.NewMockImageCache(ctl)
+	imagesServiceMock := mock_services.NewMockImages(ctl)
+	resizerService := NewResizerService(rawImageCacheMock, resizedImageCacheMock, imagesServiceMock)
+	coreImage.DecodedImage = image.Black
+
+	gomock.InOrder(
+		rawImageCacheMock.EXPECT().Remember(
+			"https://some-url.com/some-image.jpg",
+			gomock.AssignableToTypeOf(func() (*core.Image, error) { return nil, nil }),
+		).Return(coreImage, nil),
+		imagesServiceMock.EXPECT().SaveResizedImageToStorage("some-image_200x500.jpg", gomock.Any()).Return(&os.File{}, nil),
+	)
+
+	result, err := resizerService.downloadImageAndResize(input)
+	require.NoError(t, err)
+	require.NotNil(t, result)
 }
